371bd171c178ecbc0af0b11355e5714cb801cd5f
Osher Maayan <t-osmaay@microsoft.com>
2019-01-05 16:53:35 +0200

    finished up to section 7; there's probably a logical bug somewhere

---
+++ classifier.py
@@ -0,0 +1,54 @@
+import hw3_utils as utils
+
+class knn_classifier(utils.abstract_classifier):
+    def __init__(self, data, labels, k):
+        '''
+        :param k: k parameter for KNN
+        '''
+        self.k = k
+        self.dataset = data
+        self.labels = labels
+
+    def classify(self, feature_vector):
+        distances = []
+
+        for sample, tag in zip(self.dataset, self.labels):
+            distance = utils.euclidean_distance(feature_vector, sample)
+            distances.append((tag, distance))
+
+        # Sort list by distances
+        sorted_distances = sorted(distances, key=lambda dist: dist[1])
+
+        negative_count=0
+        positive_count =0
+        for i in range(self.k):
+            dataset_tag = sorted_distances[i][0]
+            if dataset_tag==True:
+                positive_count += 1
+            else:
+                negative_count += 1
+
+        if positive_count>=negative_count:
+            return True
+        else:
+            return False
+
+
+class knn_factory(utils.abstract_classifier_factory):
+    def __init__(self, k):
+        self.k = k
+
+    def train(self, data, labels):
+        return knn_classifier(data, labels, k)
+
+
+# Section 6
+csv_path = "experiments6.csv"
+k_list = [1, 3, 5, 7, 13]
+
+with open(csv_path,"w") as file:
+    for k in k_list:
+        classifier_factory = knn_factory(k)
+        accuracy, error = utils.evaluate(classifier_factory, 2)
+        output = "{k},{acc},{error}\n".format(k=k,acc=accuracy,error=error)
+        file.write(output)
--- hw3_utils.py
+++ hw3_utils.py
@@ -1,7 +1,7 @@
 data_path = r'data/Data.pickle'
 import pickle
 import numpy as np
-
+import math
 
 def load_data(path=r'data/Data.pickle'):
     '''
@@ -17,6 +17,11 @@ def load_data(path=r'data/Data.pickle'):
     return train_features, train_labels ,test_features
 
 
+# TODO: probably remove this method later
+def load_data_fold(path):
+    with open(path,'rb') as f:
+        train_features, train_labels = pickle.load(f)
+    return train_features, train_labels
 
 def write_prediction(pred, path='results.data'):
     '''
@@ -31,6 +36,22 @@ def write_prediction(pred, path='results.data'):
         output.append(l)
     with open(path, 'w') as f:
         f.write(', '.join([str(x) for x in output]) + '\n')
+
+def euclidean_distance(x, y):
+    '''
+
+    :param x: first vector of features
+    :param y: second vector of features
+    :return: the euclidean distance between the two vectors
+    '''
+    assert len(x)==len(y)
+
+    result = 0.0
+    for f1, f2 in zip(x, y):
+        result += (f1-f2)**2
+
+    return result**0.5
+
 
 
 class abstract_classifier_factory:
@@ -60,4 +81,92 @@ class abstract_classifier:
         '''
         raise Exception('Not implemented')
 
+
+def split_crosscheck_groups(dataset, num_folds):
+    '''
+
+    :param dataset:
+    :param num_folds: number of folds to divide the dataset into
+    :return: creates and saves num_folds "smaller" datasets
+    '''
+    #if len(dataset)%num_folds != 0:
+    features = dataset[0]
+    tags = dataset[1]
+    positive_samples_ind = [i for i in range(len(tags)) if tags[i]==True]
+    negative_samples_ind = [i for i in range(len(tags)) if tags[i]==False]
+
+    num_positive_samples = len(positive_samples_ind)
+    num_negative_samples = len(negative_samples_ind)
+
+    for f in range(num_folds):
+        current_features = []
+        current_tags = []
+        # Add positive samples
+        start_ind = math.floor(f*num_positive_samples / num_folds)
+        end_ind = math.floor((f+1)* num_positive_samples / num_folds)
+        for i in range(start_ind, end_ind):
+            current_features.append(features[positive_samples_ind[i]])
+            current_tags.append(tags[positive_samples_ind[i]])
+            #TODO : consider just adding True to current_tags
+
+        start_ind = f*math.floor(num_negative_samples / num_folds)
+        end_ind = (f + 1)*math.floor(num_negative_samples / num_folds)
+
+        for i in range(start_ind, end_ind):
+            current_features.append(features[negative_samples_ind[i]])
+            current_tags.append(tags[negative_samples_ind[i]])
+            #TODO : consider just adding False to current_tags
+
+        #TODO: ask if number of samples is divides by num_folds; if not - how should we proceed?
+
+        #Save fold
+        current_fold = (current_features, current_tags)
+        fold_name = "ecg_fold_{fold}.data".format(fold=f)
+        with open(fold_name, 'wb') as fold_file:
+            pickle.dump(current_fold, fold_file)
+
+    #TODO : remove later
+    print ("Done creating folds")
+
+def evaluate(classifier_factory, k):
+    '''
+
+    :param classifier_factory: creates classifiers
+    :param k:
+    :return:
+    '''
+    accuracies = []
+    errors = []
+
+    for fold in range(k):
+        valid_fold_file = "ecg_fold_{foldNum}_sub.data".format(foldNum=str(fold))
+        validation_data, validation_labels = load_data_fold(valid_fold_file)
+        N = len(validation_data)
+
+        for other_fold in range(k):
+            if other_fold!=fold:
+                train_fold_file = "ecg_fold_{foldNum}_sub.data".format(foldNum=str(other_fold))
+                train_data, train_labels = load_data_fold(train_fold_file)
+                classifier = classifier_factory.train(train_data, train_labels)
+                #TODO: see if we need to support more than 1 fold as training (concat lists)
+
+                true_classifications = 0
+                for sample,true_label in zip(validation_data, validation_labels):
+                    classification = classifier.classify(sample)
+                    if classification==true_label:
+                        true_classifications += 1
+
+                accuracy = true_classifications / N
+                accuracies.append(accuracy)
+                errors.append(1 - accuracy)
+
+    classifer_accuracy = sum(accuracies)/len(accuracies)
+    classifier_error = sum(errors)/len(errors)
+
+    return classifer_accuracy, classifier_error
+
+'''
+dataset = load_data()
+split_crosscheck_groups(dataset,2)
+'''
 
